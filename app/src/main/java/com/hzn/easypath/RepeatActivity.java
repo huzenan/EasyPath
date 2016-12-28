2006335e7949d75fa08b96fa731c16059d4f52ff
huzenan <243685305@qq.com>
2016-12-28 10:35:54 +0800

    add dynamic&repeat demo

---
+++ app/src/main/java/com/hzn/easypath/RepeatActivity.java
@@ -0,0 +1,40 @@
+package com.hzn.easypath;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+public class RepeatActivity extends FragmentActivity {
+
+    private EasyPathView epv;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repeat);
+        initViews();
+    }
+
+    private void initViews() {
+        epv = (EasyPathView) findViewById(R.id.view);
+
+        Button btnRepeat = (Button) findViewById(R.id.btn_repeat);
+        btnRepeat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                epv.startDraw(true);
+            }
+        });
+
+        Button btnStop = (Button) findViewById(R.id.btn_stop);
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                epv.stopRepeat();
+            }
+        });
+    }
+}
